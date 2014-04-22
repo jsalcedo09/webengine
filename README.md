47eb0f4198e30f45e54435b1db45b8387412b15d
Jorge Salcedo <jorge.salcedo@falconlabs.co>
2014-04-21 19:42:30 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-webengine
+WebEngine
 =========
 
 Basic Framework for App Engine based on Webapp2 integrated with Endpoints and Angular JS
---
+++ apps/test/apis.py
@@ -0,0 +1,13 @@
+import endpoints
+from settings import ENDPOINTS_AUTH_CONFIG
+from webengine.sevices import AuthRemoteService
+from protorpc import remote
+
+
+@endpoints.api(name='testApi',
+               version='v1',
+               description='testApi Endpoint',
+               auth=ENDPOINTS_AUTH_CONFIG)
+class TestApi(remote.Service, AuthRemoteService):
+    pass
+
